a4fbcb1224c67a386a8123564de5e1bd80fe4d74
gareth bk <garethbk@gmail.com>
2026-01-22 18:47:17 -0500

    lets try wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: cloudflare({
-    imageService: 'cloudflare',
-  }),
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/noop'
-    }
-  }
+  adapter: cloudflare(),
 });
